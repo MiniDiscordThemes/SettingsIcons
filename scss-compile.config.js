3f0cc245991725f72c0cf8d30dbc0f2736cf32d0
Saltssaumure <themes@saltssaumure.net>
2023-09-11 23:54:04 +0100

    Begin convert

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -1,9 +1,9 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
     meta: {
-        name: "Template" /* The official name of your theme. */,
-        scss: "temp" /* The name of your base scss file. */,
-        repo: "Template" /* The name of your theme repository. */,
+        name: "SettingsIcons" /* The official name of your theme. */,
+        scss: "main" /* The name of your base scss file. */,
+        repo: "SettingsIcons" /* The name of your theme repository. */,
         version: "dev" /* The version of your theme. */
     }
 };
