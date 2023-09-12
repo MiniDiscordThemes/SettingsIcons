d734546e87511520347683471465da4a11ccdbd7
Saltssaumure <Saltssaumure@users.noreply.github.com>
2023-09-12 03:50:44 +0000

    deploy: 651c06e7661af7e46424676c430b7e7e6d774144

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "SettingsIcons" /* The official name of your theme. */,
+        scss: "main" /* The name of your base scss file. */,
+        repo: "SettingsIcons" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
